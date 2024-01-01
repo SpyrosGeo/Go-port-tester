2bc7a339a4d148686d77dd94300ea34b7dd24975
Spyros Georgiadis <s.georgiadis@pmmretail.com>
2024-01-01 13:38:05 +0200

    refactored code

--- app.go
+++ app.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	var port string
 	flag.StringVar(&port, "p", "22", "specify port")
 	flag.Parse()
+	localIp, err := getLocalAddress()
+	if err != nil {
+		fmt.Println("Error getting local IP", err)
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+		fmt.Fprintf(w, "Test Server running on http://%s:%s", localIp, port)
 	})
-	// err:=http.ListenAndServe()
+	fmt.Printf("Started webserver on http://%s:%s\n", localIp, port)
+	err = http.ListenAndServe(localIp+":"+port, nil)
+	if err != nil {
+		fmt.Println("Error starting webServer:", err)
+	}
+}
+
+func getLocalAddress() (string, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+	localIP := conn.LocalAddr().(*net.UDPAddr)
+	return strings.Split(localIP.String(), ":")[0], nil
 }
